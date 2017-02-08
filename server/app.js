699ec54fbdac9e9d2efb0a5a74d19a7a25a06622
mac <mac@macdeMacBook-Pro.local>
2017-02-08 21:08:58 +0800

    project structure

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import environment from './configs/environment';
 import http from 'http';
+import path from 'path';
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/')));
 
 const server = http.createServer(app);
 
@@ -12,9 +15,18 @@ server.listen(environment.port, (err) => {
  console.log(`Application is running on ${environment.port}`);
 });
 
+app.route('/')
+    .get((req, res) => {
+    	res.sendFile(path.join(`${__dirname}/../client/index.html`));
+    })
+
 app.get('/', (req, res) => {
 	res.status(200).json('my name is yao ji');
 });
 
+app.get('/user', (req,res) => {
+    
+})
 
 require('./configs/db');
+
